8779f7d72342c66d280e51f971673a6ead0c56e0
Your Name <you@example.com>
2021-12-07 00:09:31 -0300

    estructura

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
     let htmlPath = path.resolve(__dirname, './views/home.html')
     res.sendFile(htmlPath) 
-}); 
+});
+
+app.get('/login', function(req, res) {
+        let htmlPath = path.resolve(__dirname, './views/login.html')
+        res.sendFile(htmlPath) 
+    });
+
+app.get('/register', function(req, res) {
+            let htmlPath = path.resolve(__dirname, './views/register.html')
+            res.sendFile(htmlPath) 
+});
